8bd8c383446d49521383e9eddd9c3ded3256c978
Casey Tomchuk <Caseytomchuk@gmail.com>
2025-02-04 16:39:09 -0500

    Task 2 - Order Details

--- cc_5.js
+++ cc_5.js
@@ -7,3 +7,14 @@ let customer = {
 for (let key in customer) {
     console.log(`${key}: ${customer[key]}`) //for each key, the name of the key is listed alongside the data in it
 };
+
+// Task 2: Object Methods
+let order = {
+    orderID: 12345,
+    totalAmount: 150,
+    status: "Processing",
+    displayOrder: function() {
+        console.log(this)
+    }
+};
+order.displayOrder();
